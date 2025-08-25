14fa5e78920f905ce7fe9043062786c568de3454
Nutodimos <oluwasomidotundavid13@gmail.com>
2025-08-25 13:52:26 +0000

    new

--- Backend/index.js
+++ Backend/index.js
@@ -1,9 +1,10 @@
 import express from "express"
-
+import notesRoutes from "./routes/notesRoutes.js"
 const app = express ();
-app.get("/api/notes",(req,res) => {
-res.send("5 notes from Nutodimos");
-});
+
+app.use("/api/notes", notesRoutes);
+
+
 app.listen(5001, () =>{
     console.log("Server started on PORT: 5001");
 });
